5cc9ac063741ab57d6a134ab096fb2701c52e354
Bublaaa <yanuarkrisna7@gmail.com>
2026-05-11 21:04:59 +0700

    Correct client sector in testimony

--- src/constants/TestimonyData.js
+++ src/constants/TestimonyData.js
@@ -1,16 +1,16 @@
 export const testimonies = [
   {
-    client: "Mining Sector Client",
+    client: "Education Sector Client",
     message:
       '"PT Garda Setia Waspada exceeded our expectations with their innovative solutions and seamless execution. A trusted partner for our critical projects."',
   },
   {
-    client: "Power Generation Client",
+    client: "Industrial Sector Client",
     message:
       '"Their technical expertise and attention to detail have been invaluable to our operations. We highly recommend PT Garda Setia Waspada for any industrial project."',
   },
   {
-    client: "Oil & Gas Sector Client",
+    client: "Residential Sector Client",
     message:
       '"Professionalism, quality, and reliability define PT Garda Setia Waspada. We look forward to many more successful projects together."',
   },
